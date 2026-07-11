383206e5322b5a148a2082235a6742603c2c67ac
Miles70 <Selimyalcinner@gmail.com>
2026-07-11 20:29:36 +0300

    Remove demo auto seed on server start

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import { createApp } from "./app.js";
 import { connectDatabase, disconnectDatabase } from "./config/database.js";
-import { syncProductsFromCatalog } from "./services/productSync.js";
 
 const port = Number(process.env.PORT) || 5000;
 const app = createApp();
@@ -9,13 +8,6 @@ let server;
 
 async function startServer() {
   await connectDatabase();
-
-  if (String(process.env.AUTO_SEED_PRODUCTS || "true").toLowerCase() === "true") {
-    const result = await syncProductsFromCatalog();
-    console.log(
-      `Product catalog synced: ${result.modifiedCount || 0} updated, ${result.upsertedCount || 0} created.`
-    );
-  }
 
   server = app.listen(port, () => {
     console.log(`Kemalreis API running on http://localhost:${port}`);
