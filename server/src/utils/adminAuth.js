d2eab69f67475ac6627bdd709be7dd6b4db2b579
Miles70 <Selimyalcinner@gmail.com>
2026-07-13 19:54:54 +0300

    Rename admin session subject to Gabaloo

--- server/src/utils/adminAuth.js
+++ server/src/utils/adminAuth.js
@@ -78,7 +78,7 @@ export function verifyAdminToken(token) {
   }
 
   return {
-    sub: "kemalreis-admin",
+    sub: "gabaloo-admin",
     email: session.email,
     iat: Math.floor(session.issuedAt / 1000),
     exp: Math.floor(session.expiresAt / 1000),
