bca311e52b13accbfa136452726d82147c244fbf
Miles70 <Selimyalcinner@gmail.com>
2026-09-24 19:07:10 +0300

    Register live travel translations

--- src/i18n/translations.js
+++ src/i18n/translations.js
@@ -15,6 +15,7 @@ import localTranslations from "./localTranslations";
 import regionalSharedTranslations from "./regionalSharedTranslations";
 import itSharedTranslations from "./itSharedTranslations";
 import hotelDetailTranslations from "./hotelDetailTranslations";
+import travelRuntimeTranslations from "./travelRuntimeTranslations";
 
 function withSharedTranslations(baseTranslations, language) {
   const regional =
@@ -36,6 +37,10 @@ function withSharedTranslations(baseTranslations, language) {
     account,
     localPage: local.localPage,
     hotelDetail: hotelDetailTranslations[language] || hotelDetailTranslations.en,
+    travelPage: {
+      ...baseTranslations.travelPage,
+      runtime: travelRuntimeTranslations[language] || travelRuntimeTranslations.en,
+    },
     checkoutPage: {
       ...baseTranslations.checkoutPage,
       ...payment.checkoutPage,
